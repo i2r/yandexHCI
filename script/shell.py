80920d426221a8ec5d159eb91eb456561a2a6a68
Alexander Korolev <samrockad@gmail.com>
2012-08-02 16:44:41 +0400

    Revert "wxtenden py script to take "verbose" option"
    This reverts commit 2976d716a462562e238995d0a1485331155bd723.

--- script/shell.py
+++ script/shell.py
@@ -5,7 +5,6 @@ import argparse
 parser = argparse.ArgumentParser(description='Print the number of arguments.')
 parser.add_argument('arguments', metavar='ARG', type=str, nargs='*', help='some arguments')
 parser.add_argument('-m', dest='message', default='', help='custom message')
-parser.add_argument('-v', action='store_true', default=False, help='Print list of argumentss', dest='verbose')
 # TODO: add '-v' option for verbose mode
 
 args = parser.parse_args()
@@ -14,9 +13,7 @@ count = 0
 
 for a in args.arguments:
 	# TODO: add '-v' option for verbose mode
-	# and print each argument
-	if (args.verbose == True):
-		print("Arg:",a)
+	# and print each argument	
 	count += 1
 
 if args.message != '':
