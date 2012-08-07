c53f93ecf18af6809db82a07fd8086bc52927418
Alexander Korolev <samrockad@gmail.com>
2012-08-07 10:51:29 +0400

    Added -v options

--- script/printargs_sh.sh
+++ script/printargs_sh.sh
@@ -8,35 +8,47 @@ Usage: printargs.sh [OPTIONS] [ARGUMENTS]
 OPTIONS:
   -h      print help message
   -m MSG  custom message
+  -v 	  print arguments
 
 Examples:
   printargs.sh a b c
   printargs.sh -m 'Arguments count: ' a b c
   printargs.sh -h
-
+  printargs.sh -v a b c
 EOF
 }
 
-while getopts "hm:" OPTION # TODO: add '-v' option for verbose mode
+while getopts "hmv:" OPTION # TODO: add '-v' option for verbose mode
 do
     case $OPTION in
         h)
             usage
             exit 1
             shift;;
+		v)
+			PRINTARGS=1			
+			shift;;
         m)
             MESSAGE=$OPTARG
-            shift;shift;;
+            shift;shift;;		
     esac
 done
 
 COUNT=0
+ARGSSTRING="Arguments: "
 
 for ARG in $@; do
     # TODO: add '-v' option for verbose mode
     # and print each argument
-    let COUNT+=1
+	if [[ $PRINTARGS ]]; then
+		ARGSSTRING+=" $ARG"
+	fi
+	let COUNT+=1
 done
+
+if [[ $PRINTARGS ]]; then
+	echo $ARGSSTRING
+fi
 
 if [[ "$MESSAGE" != "" ]]; then
     echo $MESSAGE
