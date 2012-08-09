c4b51352dee2eba3a73bc1d395f50dfd7c67cb10
Alexander Korolev <samrockad@gmail.com>
2012-08-09 22:58:32 +0400

    Filling sections

--- resumePage/js/script.js
+++ resumePage/js/script.js
@@ -1,1 +1,11 @@
 /** @author: Alexander Korolev */
+
+$(function () {
+    $(".j-box").each(function () {
+        var lock = $(this).find(".j-box-lock"), contents = $(this).find(".j-box-contents");
+        $(this).on("click", lock, function () {
+            $(this).toggleClass("b-box-opened");
+            contents.fadeToggle(200);
+        });
+    });
+});
