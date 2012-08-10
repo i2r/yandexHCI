1b67ec074e352ee06e7189578dfeff692f1bd45b
Alexander Korolev <samrockad@gmail.com>
2012-08-10 16:55:22 +0400

    Dynamic table of contents

--- resumePage/js/script.js
+++ resumePage/js/script.js
@@ -1,11 +1,76 @@
 /** @author: Alexander Korolev */
 
 $(function () {
-    $(".j-box").each(function () {
-        var lock = $(this).find(".j-box-lock"), contents = $(this).find(".j-box-contents");
-        $(this).on("click", lock, function () {
-            $(this).toggleClass("b-box-opened");
-            contents.fadeToggle(200);
-        });
-    });
+
+	/**
+	 * "Выпадающий" контейнер
+	 */
+	$(".j-box").each(function () {
+		var lock = $(this).find(".j-box-lock"),
+			contents = $(this).find(".j-box-contents");
+		$(this).on("click", lock, function () {
+			$(this).toggleClass("b-box-opened");
+			contents.fadeToggle(200);
+		});
+	});
+
+	/**
+	 * Оглавление
+	 */
+
+	var contentsContainer = $(".j-contents"), contentsData = new Contents();
+
+	$(".j-section").each(function (index) {
+		var chapters = [];
+		$(this).find(".j-chapter").each(function (index) {
+			chapters.push(new Chapter($(this).find(".j-chapter-title").text().replace(/([\r\n:]|\s{2,})/ig,""), index));
+		});
+		contentsData.sections[index] = new Section($(this).find(".j-section-title").text(), chapters);
+	});
+
+	console.log(contentsData);
+
 });
+
+/**
+ * @class
+ * @constructor
+ */
+function Contents(sections) {
+	/**
+	 * @type {Section[]}
+	 */
+	this.sections = sections || [];
+}
+
+/**
+ * @class
+ * @constructor
+ */
+function Section(name, chapters) {
+	/**
+	 * @type {String}
+	 */
+	this.name = name || "";
+
+	/**
+	 * @type {Chapter[]}
+	 */
+	this.chapters = chapters || [];
+}
+
+/**
+ * @param name
+ * @constructor
+ */
+function Chapter(name, position) {
+	/**
+	 * @type {String}
+	 */
+	this.name = name || "";
+
+	/**
+	 * @type {Number}
+	 */
+	this.position = position || 0;
+}
